dca1d9f86438c4957b0e9732fe64b46251eb4bda
Pedro Ortiz <pedroj_ortiz@outlook.com>
2020-07-04 18:44:22 -0500

    Estructura modelo definida

--- src/app/model/Rol.ts
+++ src/app/model/Rol.ts
@@ -1,22 +1,6 @@
 export class Rol {
     
-    private uid: string;
-    private descripcion: string;
-
-    public getUid(): string {
-        return this.uid;
-    }
-
-    public setUid(uid: string): void {
-        this.uid = uid;
-    }
-
-    public getDescripcion(): string {
-        return this.descripcion;
-    }
-
-    public setDescripcion(descripcion: string): void {
-        this.descripcion = descripcion;
-    }
+    uid: string;
+    descripcion: string;
     
 }
